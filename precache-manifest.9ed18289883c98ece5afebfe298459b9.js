e1f14227392498614b83c822906485519be84d12
mateen <admatinm@gmail.com>
2020-06-15 13:58:25 +0600

    Updates

---
+++ precache-manifest.9ed18289883c98ece5afebfe298459b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f964e377fe58317a0fe",
+    "url": "/thin-piece/static/js/runtime~main.e3b5502e.js"
+  },
+  {
+    "revision": "10deac193e681555b43a",
+    "url": "/thin-piece/static/js/main.163a6a11.chunk.js"
+  },
+  {
+    "revision": "087b344a65f29a849438",
+    "url": "/thin-piece/static/js/2.04249595.chunk.js"
+  },
+  {
+    "revision": "10deac193e681555b43a",
+    "url": "/thin-piece/static/css/main.72a71bde.chunk.css"
+  },
+  {
+    "revision": "a637e748488dac67024c81db48713a4f",
+    "url": "/thin-piece/index.html"
+  }
+];
